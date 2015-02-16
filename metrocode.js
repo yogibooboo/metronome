de16c82e96b3da8fa65efd0071bedec712072e64
yogibooboo <luciano.manenti@gmail.com>
2015-02-16 19:26:23 +0100

    prova10

--- metrocode.js
+++ metrocode.js
@@ -106,7 +106,7 @@ tmnoaudio=new Image();
 tmnoaudio.src="images/metronome/noaudiog.png";
 
 var buffers={};
-
+/*
 var context;
 
 
@@ -114,7 +114,7 @@ window.AudioContext = window.AudioContext||window.webkitAudioContext;
 context = new AudioContext();
 
 
-/*
+
 
 function playSound(buffer,time) {
   var source = context.createBufferSource(); // creates a sound source
