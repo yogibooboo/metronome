3e1d962185d4bd0fe90c7760dbb32141ffd5a3f9
yogibooboo <luciano.manenti@gmail.com>
2015-02-16 16:38:19 +0100

    prova2

--- metrocode.js
+++ metrocode.js
@@ -9,3 +9,8 @@ $('#bstart').click(function () {
 
 
 
+ canvasmetronomo = document.getElementById("canvasmetronomo");
+  b= canvasmetronomo.getContext('2d');
+
+b.fillStyle="#FFFFFF";
+b.fillRect(0,0,50,50);
