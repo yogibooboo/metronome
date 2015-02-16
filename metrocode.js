84ad79483bd69e18d08bf5805fd1936770c8aeae
yogibooboo <luciano.manenti@gmail.com>
2015-02-16 17:31:23 +0100

    prova 3

--- metrocode.js
+++ metrocode.js
@@ -3,14 +3,17 @@
 $('#bstart').click(function () {
 	  	$("#bstart").css({"border-color":"blue"});
    		$("#bstart").text("FATTO");
+		b.drawImage(tmmetronomo, 0, 0,375,750);
 
 
 });
 
-
+tmmetronomo=new Image();
+tmmetronomo.src="images/metronome.png";
 
  canvasmetronomo = document.getElementById("canvasmetronomo");
   b= canvasmetronomo.getContext('2d');
 
 b.fillStyle="#FFFFFF";
 b.fillRect(0,0,50,50);
+b.drawImage(tmmetronomo, 0, 0,375,750);
